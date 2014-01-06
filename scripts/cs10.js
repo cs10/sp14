7750fc4e66312886611f2790c835b6a0742bb5d7
Michael Ball <cycomachead@gmail.com>
2014-01-05 22:00:13 -0800

    js work

--- scripts/cs10.js
+++ scripts/cs10.js
@@ -1,28 +1,39 @@
 // Function used to highlight the current day.
 function updateCalendar() {
-    var today = new Date();
-    var tdate = today.getDate();
-    var tmonth = today.getMonth() + 1;
-    //var tday = today.getDay();
+    today  = new Date()
+    tdate  = today.getDate()
+    tmonth = today.getMonth() + 1;
+    tday   = today.getDay()
 
-    //console.log("TODAY: \n" + today);
-    var tableCellArr = new Array();
-    tableCellArr = document.getElementsByTagName("td");
+    tableCellArr = new Array();
+    tableCellArr = document.getElementsByClassName("cal");
     for(var i = 0; i < tableCellArr.length; i++) {
-        var tagObj = tableCellArr[i];
-        var mo = tagObj.id;
-        //console.log("id:\t" + mo)
-        var dys = tagObj.title.split(",");
-        //console.log("days:\t" + dys);
+        row = tableCellArr[i]
+        // Cells has 9 items
+        // Week, Dates, Readings, Lect, Lab, Lect, Lab, Disc, HW
+        cells = row.getElementsByTagName('td')
+        tmp = cells[1].split(" to ")[0].split('-')
+        mon = tmp[0]
+        day = tmp[1]
+
         //golden rectangle for current date
-        for (var j = 0; j < dys.length; j++) {
-            if (dys[j] == tdate && mo == tmonth) {
-                tagObj.style.border="10px solid Gold";
-            } else if (mo > 0 && dys[j] > 0 && ((mo == tmonth && dys[j] < tdate) || mo < tmonth)) {
-                tagObj.style.backgroundColor="#BABABA";
+        for (var j = 2; j < 9; j++) {
+            celldate = getCellDate(j, day)
+            if (celldate == tdate && mon == tmonth) {
+                cells[j].style.border = "10px solid Gold"
+            } else if (mon < tmonth || (mon == tmonth && celldate < tdate)) {
+                cells[j].style.backgroundColor = "#BABABA"
             }
         }
     } //closing for loop
+}
+
+function getCellDate(cellNum, date) {
+    if (cellnNum === 2) {
+        return date - 1;
+    } else {
+        return date + cellNum - 3;
+    }
 }
 
 function displaySpeech(img_name, img_src) {
